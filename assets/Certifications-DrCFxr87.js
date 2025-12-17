ea034f5f6e2312a033ffaba0eed0213a8a75c020
imrajeevnayan <immrrobot121@gmail.com>
2025-12-17 12:07:04 +0530

    Updates

--- assets/Certifications-DrCFxr87.js
+++ assets/Certifications-DrCFxr87.js
@@ -1,4 +1,4 @@
-import{c as s,j as e,m as t}from"./index-BOZprn57.js";/**
+import{c as s,j as e,m as t}from"./index-Q8xMzbfl.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/Skills-B2b8ApYK.js
+++ assets/Skills-B2b8ApYK.js
@@ -1,4 +1,4 @@
-import{c as i,j as e,m as s}from"./index-BOZprn57.js";/**
+import{c as i,j as e,m as s}from"./index-Q8xMzbfl.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
